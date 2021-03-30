08e46cc1c4b6acec4e470e45cbfcf7feff151789
prabhu1983 <63340564+prabhu1983@users.noreply.github.com>
2021-03-30 15:46:34 +0530

    Update severity.models.class.ts

--- src/app/bugs/models/classes/severity.models.class.ts
+++ src/app/bugs/models/classes/severity.models.class.ts
@@ -6,8 +6,9 @@ export class SeverityModel {
     private PRBID;
     private StoryID;
     private Description;
+    private Chapter;
 
-    constructor(severity, status, onTrack, createdDate, prbID, storyID, description) {
+    constructor(severity, status, onTrack, createdDate, prbID, storyID, description, chapter) {
         this.Severity = severity;
         this.Status = status;
         this.OnTrack = onTrack;
@@ -15,5 +16,6 @@ export class SeverityModel {
         this.PRBID = prbID;
         this.StoryID = storyID;
         this.Description = description;
+        this.Chapter = chapter;
     }
 }
