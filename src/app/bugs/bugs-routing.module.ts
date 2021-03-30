f151e62a5112adfd9d4e8463e18391c560dc8f66
prabhu1983 <63340564+prabhu1983@users.noreply.github.com>
2021-03-30 15:37:17 +0530

    Add files via upload

--- src/app/bugs/bugs-routing.module.ts
+++ src/app/bugs/bugs-routing.module.ts
@@ -1,8 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { BugsDashboardComponent } from './components/bugs-dashboard/bugs-dashboard.component';
+import { BugsTechnologyComponent } from './components/bugs-technology/bugs-technology.component';
 
-const routes: Routes = [{path: 'bugsdashboard', component: BugsDashboardComponent}];
+const routes: Routes = [
+  {path: 'bugsdashboard', component: BugsDashboardComponent}, 
+  {path: "bugstechnology", component: BugsTechnologyComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
--- src/app/bugs/bugs.module.ts
+++ src/app/bugs/bugs.module.ts
@@ -3,13 +3,14 @@ import { CommonModule } from '@angular/common';
 
 import { BugsRoutingModule } from './bugs-routing.module';
 import { BugsDashboardComponent } from './components/bugs-dashboard/bugs-dashboard.component';
+import { BugsTechnologyComponent } from './components/bugs-technology/bugs-technology.component';
 import { CommonsModule } from '../commons/commons.module';
 import { BugsMapperService } from './services/bugs-mapper.service';
 import { BugsGenericService } from './services/bugs-generic.service';
 
 // import { AgGridModule } from 'ag-grid-angular';
 @NgModule({
-  declarations: [BugsDashboardComponent],
+  declarations: [BugsDashboardComponent, BugsTechnologyComponent],
   imports: [
     CommonModule,
     BugsRoutingModule,
